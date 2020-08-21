188bb8651aadfb543753c34c74cba66548a49366
Ahmet Mirza Yildirim <ayildirim@PROTEL.COM.TR>
2020-08-21 16:41:27 +0300

    Project Upload Commit

---
+++ ExcelExportTraining/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using ClosedXML.Excel;
+using ExcelExportTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExcelExportTraining.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View( GetCustomer() );
+        }
+
+        public List<Customer> GetCustomer()
+        {
+            List<Customer> CustomerList = new List<Customer>();
+
+            for (int i = 1; i <= 50; i++)
+            {
+                CustomerList.Add
+                    (
+                        new Customer
+                        {   ID = i,
+                            Name = "Name - " + i,
+                            Surname = "Surname - " + i,
+                            Phone = i.ToString()
+                        }
+                    );
+            }
+
+            return CustomerList;
+        }
+
+        public FileStreamResult ExportToExcel(string id)
+        {
+            DataTable dt = new DataTable();
+         
+            dt.Columns.AddRange
+           (
+               new DataColumn[3]
+               {
+                            new DataColumn("Isim"),
+                            new DataColumn("Soyisim"),
+                            new DataColumn("Telefon")
+               }
+           );
+
+            dt.TableName = "Müşteri Listesi"; // Excel içinde ki sayfada görüntülenecek isim
+            List<Customer> CustomerList = GetCustomer();
+            int rowIndex = 0;
+
+            foreach (var Customer in CustomerList)
+            {
+                dt.Rows.Add();
+                dt.Rows[rowIndex]["Isim"] = Customer.Name;
+                dt.Rows[rowIndex]["Soyisim"] = Customer.Surname;
+                dt.Rows[rowIndex]["Telefon"] = Customer.Phone;
+
+                rowIndex++;
+            }
+
+            // dosya ismi hazırlanıyor . aykırı karakterler temizleniyor..
+            string fileName = ("Müşteri Listesi-" + DateTime.Now.ToString()).Replace('/', '_').Replace(':', '.').Replace(' ', '_') + ".xlsx";
+
+
+            using (var workbook = new XLWorkbook())
+            {
+                MemoryStream Ms = new MemoryStream();
+
+                var ws = workbook.Worksheets.Add(dt);
+                ws.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center); // öğeler yatayda ortalanıyor
+                ws.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Center); // öğeler dikeyde ortalanıyor
+               
+                /*
+                 Satır ve sütunların boyutu içeriklere göre ayarlanıyor.
+                 */
+                ws.Rows().AdjustToContents();
+                ws.Columns().AdjustToContents();
+              
+                /*
+                 false => Bütün kolonlar için otomatik filtreleme kapatılıyor
+                 true => Bütün kolonlar için otomatik filtreleme aktif oluyor(varsayılan değer)
+                */
+                ws.Tables.FirstOrDefault().ShowAutoFilter = false;
+
+                workbook.SaveAs(Ms);
+                Ms.Position = 0;
+
+                // File stream olarak sonuç döndürülüyor.
+                return new FileStreamResult(Ms, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") { FileDownloadName = fileName };
+            }
+        }
+    }
+}
---
+++ ExcelExportTraining/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExcelExportTraining")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExcelExportTraining")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b568a844-d884-4b95-9406-c27a26b23234")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
